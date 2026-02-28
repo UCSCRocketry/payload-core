54d4e81b5e7fd5614324d7fbc8316f15fbacdd22
kchen3157 <grubby-choral-0a@icloud.com>
2026-02-28 10:12:44 -0800

    core: update main's spi3 settings for altimeter

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 
 #include "main.h"
+#include "stm32f4xx_hal_spi.h"
 
 CRC_HandleTypeDef hcrc;
 
@@ -255,10 +256,10 @@ static void MX_SPI3_Init(void)
 	hspi3.Init.Mode = SPI_MODE_MASTER;
 	hspi3.Init.Direction = SPI_DIRECTION_2LINES;
 	hspi3.Init.DataSize = SPI_DATASIZE_8BIT;
-	hspi3.Init.CLKPolarity = SPI_POLARITY_LOW;
-	hspi3.Init.CLKPhase = SPI_PHASE_1EDGE;
+	hspi3.Init.CLKPolarity = SPI_POLARITY_HIGH;
+	hspi3.Init.CLKPhase = SPI_PHASE_2EDGE;
 	hspi3.Init.NSS = SPI_NSS_SOFT;
-	hspi3.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+	hspi3.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_8;
 	hspi3.Init.FirstBit = SPI_FIRSTBIT_MSB;
 	hspi3.Init.TIMode = SPI_TIMODE_DISABLE;
 	hspi3.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
