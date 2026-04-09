27e7f00dd7363834bf25b8cb05c53967590a6704
kchen3157 <grubby-choral-0a@icloud.com>
2026-04-08 18:09:28 -0700

    core: enable TIM1 update interrupts

--- src/main.c
+++ src/main.c
@@ -52,6 +52,8 @@ int main(void)
 	MX_SPI1_Init();
 	MX_CRC_Init();
 
+  HAL_TIM_Base_Start_IT(&htim1);
+
 	log_init(&huart2);
 
 	payload_run();
@@ -325,6 +327,7 @@ static void MX_TIM1_Init(void)
 
 	/* USER CODE END TIM1_Init 0 */
 
+	TIM_ClockConfigTypeDef sClockSourceConfig = { 0 };
 	TIM_MasterConfigTypeDef sMasterConfig = { 0 };
 	TIM_OC_InitTypeDef sConfigOC = { 0 };
 	TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = { 0 };
@@ -333,12 +336,21 @@ static void MX_TIM1_Init(void)
 
 	/* USER CODE END TIM1_Init 1 */
 	htim1.Instance = TIM1;
-	htim1.Init.Prescaler = 0;
+	htim1.Init.Prescaler = 258;
 	htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
 	htim1.Init.Period = 65535;
 	htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
 	htim1.Init.RepetitionCounter = 0;
 	htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+	if (HAL_TIM_Base_Init(&htim1) != HAL_OK)
+	{
+		Error_Handler();
+	}
+	sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+	if (HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig) != HAL_OK)
+	{
+		Error_Handler();
+	}
 	if (HAL_TIM_PWM_Init(&htim1) != HAL_OK)
 	{
 		Error_Handler();
