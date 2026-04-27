8993c0aabf5b7526ecd5d272f7eadfb36c79e409
Alejandro Estrella <aqealejandro@gmail.com>
2026-04-27 12:41:32 -0700

    fix freezing on spi flash

--- src/stm32f4xx_hal_msp.c
+++ src/stm32f4xx_hal_msp.c
@@ -472,7 +472,7 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef *htim_base)
 		/* Peripheral clock enable */
 		__HAL_RCC_TIM1_CLK_ENABLE();
 		/* TIM1 interrupt Init */
-		HAL_NVIC_SetPriority(TIM1_UP_TIM10_IRQn, 1, 0);
+		HAL_NVIC_SetPriority(TIM1_UP_TIM10_IRQn, 0, 0);
 		HAL_NVIC_EnableIRQ(TIM1_UP_TIM10_IRQn);
 		/* USER CODE BEGIN TIM1_MspInit 1 */
 
@@ -486,7 +486,7 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef *htim_base)
 		/* Peripheral clock enable */
 		__HAL_RCC_TIM2_CLK_ENABLE();
 		/* TIM2 interrupt Init */
-		HAL_NVIC_SetPriority(TIM2_IRQn, 0, 0);
+		HAL_NVIC_SetPriority(TIM2_IRQn, 1, 0);
 		HAL_NVIC_EnableIRQ(TIM2_IRQn);
 		/* USER CODE BEGIN TIM2_MspInit 1 */
 
